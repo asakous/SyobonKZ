9326135cd92fbb1a9e0764c8c43c0f4c8c1322aa
+KZ <60852359+M0REKZ@users.noreply.github.com>
2026-06-15 09:02:08 -0300

    OpenSyobonAction v0.9

---
+++ DxLib.cpp
@@ -0,0 +1,222 @@
+#include "DxLib.h"
+
+bool keysHeld[SDLK_LAST];
+int DxLib_Init()
+{
+    setlocale(LC_CTYPE, "en_ca.UTF-8");
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+	printf("Unable to init SDL: %s\n", SDL_GetError());
+	return -1;
+    }
+
+    if (!
+	(screen =
+	 SDL_SetVideoMode(480 /*(int)fmax/100 */ ,
+			  420 /*(int)fymax/100 */ , 32,
+			  SDL_SWSURFACE | SDL_DOUBLEBUF))) {
+	SDL_Quit();
+	return -1;
+    }
+
+    SDL_WM_SetCaption("Syobon Action (しょぼんのアクション)",
+		      NULL);
+    SDL_ShowCursor(SDL_DISABLE);
+
+    //Initialize font
+    if (TTF_Init() == -1) {
+	printf("Unable to init SDL_ttf: %s\n", TTF_GetError());
+	return -1;
+    }
+
+    if (font == NULL) {
+	printf("Unable to load font: %s\n", TTF_GetError());
+	return -1;
+    }
+    //Audio Rate, Audio Format, Audio Channels, Audio Buffers
+#define AUDIO_CHANNELS 4
+    if (Mix_OpenAudio(22050, AUDIO_S16SYS, AUDIO_CHANNELS, 1024)) {
+	printf("Unable to init SDL_mixer: %s\n", Mix_GetError());
+	return -1;
+    }
+
+    for (int i = 0; i < SDLK_LAST; i++)
+	keysHeld[i] = false;
+    for (int i = 0; i < FONT_MAX; i++)
+	font[i] = NULL;
+    srand(time(NULL));
+
+    return 0;
+}
+
+//Main screen
+SDL_Surface *screen;
+
+//Fonts
+byte fontsize = 0;
+TTF_Font *font[FONT_MAX];
+
+//Strings
+void SetFontSize(byte size)
+{
+    fontsize = size;
+    if (font[size] == NULL) {
+	font[size] = TTF_OpenFont("res/sazanami-gothic.ttf", size);
+	if (font[size] == NULL) {
+	    printf("Unable to load font: %s\n", TTF_GetError());
+	    exit(1);
+	}
+    }
+}
+
+byte fontType = DX_FONTTYPE_NORMAL;
+void ChangeFontType(byte type)
+{
+    fontType = type;
+}
+
+void DrawString(int a, int b, const char *x, Uint32 c)
+{
+    SDL_Color color = { c >> 16, c >> 8, c };
+    SDL_Surface *rendered = TTF_RenderUTF8_Solid(font[fontsize], x, color);
+    if (fontType == DX_FONTTYPE_EDGE) {
+	SDL_Color blk = { 0, 0, 0 };
+	SDL_Surface *shadow = TTF_RenderUTF8_Solid(font[fontsize], x, blk);
+	for (int i = a - 1; i <= a + 1; i++)
+	    for (int j = b - 1; j <= b + 1; j++) {
+		DrawGraphZ(i, j, shadow);
+	    }
+	SDL_FreeSurface(shadow);
+    }
+    DrawGraphZ(a, b, rendered);
+    SDL_FreeSurface(rendered);
+}
+
+void DrawFormatString(int a, int b, Uint32 color, const char *str, ...)
+{
+    va_list args;
+    char *newstr = new char[strlen(str) + 16];
+    va_start(args, str);
+    vsprintf(newstr, str, args);
+    va_end(args);
+    DrawString(a, b, newstr, color);
+    delete newstr;
+}
+
+//void DrawFormatString(int a, int b, int c
+
+//Key Aliases
+#define KEY_INPUT_ESCAPE SDLK_ESCAPE
+
+bool ex = false;
+
+void UpdateKeys()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+	switch (event.type) {
+	case SDL_KEYDOWN:
+	    keysHeld[event.key.keysym.sym] = true;
+	    break;
+	case SDL_KEYUP:
+	    keysHeld[event.key.keysym.sym] = false;
+	    break;
+	case SDL_QUIT:
+	    ex = true;
+	    break;
+	}
+    }
+}
+
+byte ProcessMessage()
+{
+    return ex;
+}
+
+byte CheckHitKey(int key)
+{
+    return keysHeld[key];
+}
+
+byte WaitKey()
+{
+    SDL_Event event;
+    while (true) {
+	while (SDL_PollEvent(&event))
+	    if (event.type == SDL_KEYDOWN)
+		return event.key.keysym.sym;
+    }
+}
+
+/*Uint32 GetColor(byte r, byte g, byte b)
+{
+    return r << 8 * 3 | g << 8 * 2 | b << 8 | 0xFF;
+}*/
+
+void DrawGraphZ(int a, int b, SDL_Surface * mx)
+{
+    SDL_Rect offset;
+    offset.x = a;
+    offset.y = b;
+    SDL_BlitSurface(mx, NULL, screen, &offset);
+}
+
+void DrawTurnGraphZ(int a, int b, SDL_Surface * mx)
+{
+    SDL_Rect offset;
+    offset.x = a;
+    offset.y = b;
+
+    SDL_Surface *flipped = zoomSurface(mx, -1, 1, 0);
+    SDL_SetColorKey(flipped, SDL_SRCCOLORKEY,
+		    SDL_MapRGB(flipped->format, 9 * 16 + 9, 255, 255));
+    SDL_BlitSurface(flipped, NULL, screen, &offset);
+    SDL_FreeSurface(flipped);
+}
+
+void DrawVertTurnGraph(int a, int b, SDL_Surface * mx)
+{
+    SDL_Rect offset;
+    offset.x = a - mx->w / 2;
+    offset.y = b - mx->h / 2;
+
+    SDL_Surface *flipped = rotozoomSurface(mx, 180, 1, 0);
+    SDL_SetColorKey(flipped, SDL_SRCCOLORKEY,
+		    SDL_MapRGB(flipped->format, 9 * 16 + 9, 255, 255));
+    SDL_BlitSurface(flipped, NULL, screen, &offset);
+    SDL_FreeSurface(flipped);
+}
+
+SDL_Surface *DerivationGraph(int srcx, int srcy, int width, int height,
+			     SDL_Surface * src)
+{
+    SDL_Surface *img =
+	SDL_CreateRGBSurface(SDL_SWSURFACE, width, height,
+			     screen->format->BitsPerPixel,
+			     src->format->Rmask, src->format->Bmask,
+			     src->format->Gmask, src->format->Amask);
+
+    SDL_Rect offset;
+    offset.x = srcx;
+    offset.y = srcy;
+    offset.w = width;
+    offset.h = height;
+
+    SDL_BlitSurface(src, &offset, img, NULL);
+    SDL_SetColorKey(img, SDL_SRCCOLORKEY,
+		    SDL_MapRGB(img->format, 9 * 16 + 9, 255, 255));
+    return img;
+}
+
+//Noticably different than the original
+SDL_Surface *LoadGraph(const char *filename)
+{
+    SDL_Surface *image = IMG_Load(filename);
+
+    if (image) {
+	return image;
+    } else {
+	fprintf(stderr, "Error: Unable to load %s\n", filename);
+	exit(1);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,30 @@
+Open Syobon Action v0.9
+------------------
+
+Open Syobon Action is a cross-platform port of the unforgiving Japanese parody of
+Super Mario Bros..
+
+Requires:
+SDL, SDL_image, SDL_mixer, SDL_ttf, SDL_gfx
+
+Controls:
+Z, Up - Jump
+Left, Right - Movement
+Down - Enter warp pipe
+0-9 (On menu) - Level Select
+
+Unimplemented (Todo):
+*Gamepad support
+*Font thickness
+*Dynamic audio volume
+
+--Changelog--
+Open Syobon Action v0.9:
+*Fixed incorrect character rendering, uses font very similar to the original game
+*Changed encoding of the original readme and source code to UTF-8, so they can
+ be viewed correctly with most software
+*Changed DOS newlines to Unix newlines in source and readme
+*Source code has been cleaned up with "indent", removed a few obsolete comments
+
+Open Syobon Action v0.8:
+*First release
